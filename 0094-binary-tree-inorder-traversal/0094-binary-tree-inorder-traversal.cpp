930017c57a7a0d690c93e54ebcb9ce4ccaf7c0be
kasthaa <kastha664@gmail.com>
2026-08-14 13:24:32 +0530

    Time: 0 ms (100%), Space: 10.7 MB (89.58%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,27 +6,21 @@ public:
 
         while (cur != NULL) {
 
-            // If there is no left subtree
             if (cur->left == NULL) {
                 inorder.push_back(cur->val);
                 cur = cur->right;
             }
-
             else {
-                // Find the inorder predecessor
                 TreeNode* prev = cur->left;
 
                 while (prev->right != NULL && prev->right != cur) {
                     prev = prev->right;
                 }
 
-                // Create a thread
                 if (prev->right == NULL) {
                     prev->right = cur;
                     cur = cur->left;
                 }
-
-                // Remove the thread
                 else {
                     prev->right = NULL;
                     inorder.push_back(cur->val);
